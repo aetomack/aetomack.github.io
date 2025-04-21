7cff89fa791832ea58552b6b0ae3a97c1b028a37
Alex Tomack <alexander.e.tomack@vanderbilt.edu>
2025-04-20 23:09:07 -0500

    base version

--- tabsydemo/src/app/Navbar.js
+++ tabsydemo/src/app/Navbar.js
@@ -1,16 +1,32 @@
 import React from 'react'
 import Image from 'next/image'
+import './Navbar.css';
 
 const Navbar = () => {
     return (
 <nav className = "navbar">
     <div className="navbar-left">
-        <Image 
-            src="/favicon.ico"
-            width={500}
-            height={500}
-            alt="tabsy.ai"
-        />
+        <ul className="navbar-left-links">
+            <li><a href="#Demo">Demo</a></li>
+            <li><a href="#About">About</a></li>
+            <li><a href="#Features">Features</a></li>
+            <li><a href="#Testimonials">Testimonials</a></li>
+        </ul>
+    </div>
+    <div className="navbar-center">
+        <div className="navbar-logo">
+            <Image 
+                    src="/favicon.ico"
+                    width={50}
+                    height={50}
+                    alt="tabsy.ai"
+                />
+        </div>
+    </div>
+    <div className="navbar-right">
+        <ul className="navbar-right-links">
+            <li><a href="#Contact">Contact Us</a></li>
+        </ul>
     </div>
 </nav>
     )
