c833a913e743d45fb3171c018c8b542b7c622240
Alex Tomack <alexander.e.tomack@vanderbilt.edu>
2025-04-21 00:43:50 -0500

    revamp

--- tabsydemo/src/app/Navbar.js
+++ tabsydemo/src/app/Navbar.js
@@ -4,31 +4,17 @@ import './Navbar.css';
 
 const Navbar = () => {
     return (
-<nav className = "navbar">
-    <div className="navbar-left">
-        <ul className="navbar-left-links">
-            <li><a href="#Demo">Demo</a></li>
-            <li><a href="#About">About</a></li>
-            <li><a href="#Features">Features</a></li>
-            <li><a href="#Testimonials">Testimonials</a></li>
-        </ul>
-    </div>
-    <div className="navbar-center">
-        <div className="navbar-logo">
-            <Image 
-                    src="/favicon.ico"
-                    width={50}
-                    height={50}
+        <nav className = "navbar">
+            <div className="logo">
+                <Image 
+                    src="/Tabsy_AI.png"
+                    width={200}
+                    height={400}
                     alt="tabsy.ai"
                 />
-        </div>
-    </div>
-    <div className="navbar-right">
-        <ul className="navbar-right-links">
-            <li><a href="#Contact">Contact Us</a></li>
-        </ul>
-    </div>
-</nav>
+            </div>
+            <div className = "navbar-contact">CONTACT US</div>
+        </nav>
     )
 }
 
--- tabsydemo/src/app/page.tsx
+++ tabsydemo/src/app/page.tsx
@@ -1,13 +1,10 @@
-import Image from "next/image";
 import Navbar from "./Navbar.js";
-import Headline from "./Headline.js";
 
 export default function Home() {
   return (
     <div>
       <main>
         <Navbar />
-        <Headline />
       </main>
       <footer>
         
