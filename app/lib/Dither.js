6812f3f5f56cd0722bfbec97c793c77222a1bede
Alex Tomack <atomack315@gmail.com>
2025-09-23 22:50:54 -0500

    adjusted deps

--- app/lib/Dither.js
+++ app/lib/Dither.js
@@ -5,7 +5,7 @@ import { EffectComposer, wrapEffect } from '@react-three/postprocessing';
 import { Effect } from 'postprocessing';
 import * as THREE from 'three';
 
-import '../css/Dither';
+import 'dither.css';
 
 const waveVertexShader = `
 precision highp float;
