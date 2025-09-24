d93fcc7d6bb9afd92f1fb1d2281a453406a01c6f
Alex Tomack <atomack315@gmail.com>
2025-09-23 22:29:05 -0500

    bg test

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
+import Dither from "../public/assets/js/Dither.js";
 
 const Page: React.FC = () => {
   return (
@@ -149,7 +150,18 @@ const Page: React.FC = () => {
       </div>
 
       {/* BG */}
-      <div id="bg"></div>
+      <div style={{ width: '100%', height: '600px', position: 'relative' }}>
+        <Dither
+          waveColor={[0.5, 0.5, 0.5]}
+          disableAnimation={false}
+          enableMouseInteraction={true}
+          mouseRadius={0.3}
+          colorNum={4}
+          waveAmplitude={0.3}
+          waveFrequency={3}
+          waveSpeed={0.05}
+        />
+      </div>
     </div>
   );
 };
