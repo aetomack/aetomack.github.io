fb95105514c5e777aaf33f167c63bfd6cf412c9d
Alex Tomack <atomack315@gmail.com>
2025-09-27 20:51:18 -0500

    bg

--- app/page.tsx
+++ app/page.tsx
@@ -14,16 +14,27 @@ const Page: React.FC = () => {
     <div className="is-preload " >
       {/*Dither background*/}
       <div id="bg">
-        <Dither
-          waveSpeed={0.05}
-          waveFrequency={3}
-          waveAmplitude={0.3}
-          waveColor={[0.5, 0.5, 0.5]}
-          colorNum={4}
-          pixelSize={2}
-          enableMouseInteraction={true}
-          mouseRadius={1}
-        />
+       <FaultyTerminal
+        scale={1.5}
+        gridMul={[2, 1]}
+        digitSize={1.2}
+        timeScale={1}
+        pause={false}
+        scanlineIntensity={1}
+        glitchAmount={1}
+        flickerAmount={1}
+        noiseAmp={1}
+        chromaticAberration={0}
+        dither={0}
+        curvature={0}
+        tint="#ffffff"
+        mouseReact={true}
+        mouseStrength={0.5}
+        pageLoadAnimation={false}
+        brightness={1}
+        className=""
+        style={{}}
+      />
       </div>
       {/* Wrapper */}
       <div id="wrapper">
