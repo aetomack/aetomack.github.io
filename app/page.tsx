86a5c2325f30e4c0afc95860dbd9b7cbcfb737f8
aetomack <atomack315@gmail.com>
2026-07-28 18:20:38 -0700

    Add door portal component. All of the logic lives within this single component, because it wont be used anywhere else. Gateway behavior

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,5 @@
+import DoorPortal from "@/app/components/DoorPortal";
+
 export default function Home() {
-  return (
-    <div className="flex flex-1 items-center justify-center bg-cream">
-      <button
-        type="button"
-        className="font-sans text-sm font-light tracking-[0.3em] text-espresso/40 uppercase transition-colors duration-500 hover:text-espresso/80"
-      >
-        Enter
-      </button>
-    </div>
-  );
+  return <DoorPortal />;
 }
